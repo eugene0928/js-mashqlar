efb0cb968473a0f1a0bdd13981deb710f6ad6636
eugene0928 <umid09tuxtayev@gmail.com>
2022-02-23 21:55:48 +0500

    post flag

--- fs/students.js
+++ fs/students.js
@@ -1,3 +1,22 @@
+const fs = require('fs')
 const [, , flag, studentIdOrName, newUserName] = process.argv
+let students = fs.readFileSync('students_info.json', 'utf-8')
+students = JSON.parse(students.trim() || null) || []
 
-if(flag == 'PUT')
+if(flag == 'POST' && typeof studentIdOrName == 'string' && !newUserName) {
+    let user = students.find( el => el.name == studentIdOrName)
+    if(user) {
+        console.log('This user is already exists!')
+        return
+    }
+    
+        students.push({
+            id: students.length == 0 ? 1 : students.at(-1).id + 1,
+            name: studentIdOrName
+        })
+    
+
+    fs.writeFileSync('students_info.json', JSON.stringify(students, null, 4))
+
+    console.log('User is added successfully!')
+}
